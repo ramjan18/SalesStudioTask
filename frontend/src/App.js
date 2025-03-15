eeee69e26a1a91a3f04c2f06e537840d5ca9a9b8
ramjan18 <144691327+ramjan18@users.noreply.github.com>
2025-03-15 16:25:08 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://salesstudiotask.onrender.com';
 
 function App() {
     const [coupon, setCoupon] = useState(null);
